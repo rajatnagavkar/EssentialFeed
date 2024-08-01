744e1c0a96d397ab11d9d0df8e9aadb985ae711b
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2024-08-01 14:46:08 -0400

    Create FeedItem Model

---
+++ EssentailFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentailFeed
+//
+//  Created by Rajat Nagavkar on 8/1/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageUrl: URL
+}
