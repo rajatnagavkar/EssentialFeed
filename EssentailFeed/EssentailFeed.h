b9486cf5e41a6d537fe289d628da794767854105
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2024-08-01 14:42:26 -0400

    Create EssentialFeed Framework

---
+++ EssentailFeed/EssentailFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentailFeed.h
+//  EssentailFeed
+//
+//  Created by Rajat Nagavkar on 8/1/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentailFeed.
+FOUNDATION_EXPORT double EssentailFeedVersionNumber;
+
+//! Project version string for EssentailFeed.
+FOUNDATION_EXPORT const unsigned char EssentailFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentailFeed/PublicHeader.h>
+
+
