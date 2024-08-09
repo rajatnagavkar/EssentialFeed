ba9db1ec3ee25148c84def6e246ff54cfe01247e
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2024-08-09 15:29:34 -0400

    Remove LoadFeedResult extension conformance from production since its test only requirement

--- EssentailFeedTests/FeedApi/RemoteFeedLoaderTests.swift
+++ EssentailFeedTests/FeedApi/RemoteFeedLoaderTests.swift
@@ -155,16 +155,33 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     func expect(_ sut: RemoteFeedLoader,
-                toCompleteWithResult result: RemoteFeedLoader.Result,
+                toCompleteWithResult expectedResult: RemoteFeedLoader.Result,
                 when action: () -> Void,file: StaticString = #file,
                 line: UInt = #line){
         
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load {capturedResults.append($0)}
+        let exp = expectation(description: "Waiting for load completion")
+        
+        sut.load { receivedResult in
+            switch(receivedResult,expectedResult) {
+            case let (.success(receivedResult),.success(expectedResult)):
+                
+                XCTAssertEqual(receivedResult, expectedResult,file: file,line: line)
+                
+            case let (.failure(receivedError),.failure(expectedError)):
+                
+                XCTAssertEqual(receivedError, expectedError,file: file, line: line)
+                
+            default:
+                XCTFail("Expected result \(expectedResult) got \(receivedResult) instead",file: file,line: line)
+            }
+            
+            exp.fulfill()
+            
+        }
         
         action()
         
-        XCTAssertEqual(capturedResults,[result],file: file,line: line)
+        wait(for: [exp], timeout: 1.0)
         
     }
     
