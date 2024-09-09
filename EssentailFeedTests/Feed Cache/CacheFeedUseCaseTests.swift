1ba1c93bbcbb1ed5ddd6ca0465a0e6e54931cf03
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2024-09-09 12:22:21 -0400

    Move 'LocalFeedLoader' (and FeedStore collaborator) to its own file in production

--- EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentailFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -8,37 +8,9 @@
 import XCTest
 import EssentailFeed
 
-class LocalFeedLoader {
-    private let store: FeedStore
-    private let currentDate: () -> Date
-    init(store: FeedStore,currentDate: @escaping () -> Date) {
-        self.store = store
-        self.currentDate = currentDate
-    }
-    
-    func save(_ items: [FeedItem],completion: @escaping (Error?) -> Void) {
-        store.deleteCachedFeed { [weak self] error in
-            guard let self = self else {return}
-            if let cacheDeletionError = error {
-                completion(cacheDeletionError)
-            } else {
-                self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
-                    guard self != nil else {return}
-                    completion(error)
-                }
-            }
-        
-        }
-    }
-}
 
-protocol FeedStore {
-    typealias DeletionCompletion = (Error?) ->  Void
-    func deleteCachedFeed(completion: @escaping DeletionCompletion)
-    
-    typealias InsertionCompletion = (Error?)-> Void
-    func insert(_ items: [FeedItem],timestamp: Date,completion: @escaping InsertionCompletion)
-}
+
+
 
 
 
