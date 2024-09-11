86d495085ab70ec5c4939e5d15dbf72a586b140c
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2024-09-11 16:17:02 -0400

    Extract FeedCatchSpy into a shared scope to remove duplication

---
+++ EssentailFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,51 @@
+//
+//  FeedStoreSpy.swift
+//  EssentailFeedTests
+//
+//  Created by Rajat Nagavkar on 9/11/24.
+//
+
+import Foundation
+import EssentailFeed
+
+ class FeedStoreSpy: FeedStore{
+    typealias DeletionCompletion = (Error?) ->  Void
+    typealias InsertionCompletion = (Error?)-> Void
+    
+    enum ReceivedMessages: Equatable {
+        case deleteCachedFeed
+        case insert([LocalFeedImage], Date)
+    }
+    
+    private(set) var receivedMessages = [ReceivedMessages]()
+    
+    private var deletionCompletion = [DeletionCompletion]()
+    private var insertionCompletion = [InsertionCompletion]()
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        deletionCompletion.append(completion)
+        receivedMessages.append(.deleteCachedFeed)
+    }
+    
+    func completeDeletion(with error: Error, at index: Int = 0 ){
+        deletionCompletion[index](error)
+    }
+    
+    func completeDeletionSuccessful(at index: Int = 0){
+        deletionCompletion[index](nil)
+    }
+    
+    func insert(_ feed: [LocalFeedImage],timestamp: Date,completion: @escaping InsertionCompletion) {
+        insertionCompletion.append(completion)
+        receivedMessages.append(.insert(feed, timestamp))
+    }
+    
+    func completeInsertion(with error: Error,  at index: Int = 0 ) {
+        insertionCompletion[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0){
+        insertionCompletion[index](nil)
+    }
+    
+}
--- EssentailFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentailFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -24,47 +24,7 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         return (sut,store)
     }
     
-    private class FeedStoreSpy: FeedStore{
-        typealias DeletionCompletion = (Error?) ->  Void
-        typealias InsertionCompletion = (Error?)-> Void
-        
-        enum ReceivedMessages: Equatable {
-            case deleteCachedFeed
-            case insert([LocalFeedImage], Date)
-        }
-        
-        private(set) var receivedMessages = [ReceivedMessages]()
-        
-        private var deletionCompletion = [DeletionCompletion]()
-        private var insertionCompletion = [InsertionCompletion]()
-        
-        func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-            deletionCompletion.append(completion)
-            receivedMessages.append(.deleteCachedFeed)
-        }
-        
-        func completeDeletion(with error: Error, at index: Int = 0 ){
-            deletionCompletion[index](error)
-        }
-        
-        func completeDeletionSuccessful(at index: Int = 0){
-            deletionCompletion[index](nil)
-        }
-        
-        func insert(_ feed: [LocalFeedImage],timestamp: Date,completion: @escaping InsertionCompletion) {
-            insertionCompletion.append(completion)
-            receivedMessages.append(.insert(feed, timestamp))
-        }
-        
-        func completeInsertion(with error: Error,  at index: Int = 0 ) {
-            insertionCompletion[index](error)
-        }
-        
-        func completeInsertionSuccessfully(at index: Int = 0){
-            insertionCompletion[index](nil)
-        }
-        
-    }
+   
     
     
 }
