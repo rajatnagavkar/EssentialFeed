655f2cae1219213c31f56e8771d0e49876e96966
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2024-09-23 15:25:11 -0400

    Retrive delivers error on retrival error(invalid cached data)

--- EssentailFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentailFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -38,23 +38,28 @@ class CodableFeedStore {
     }
     
     private let storeURL: URL
-
+    
     
     init(storeURL: URL)
     {
         self.storeURL = storeURL
     }
-
+    
     
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion){
         guard let data = try? Data(contentsOf: storeURL) else  {
             return completion(.empty)
         }
-        let decoder = JSONDecoder()
-        let cache = try! decoder.decode(Cache.self, from: data)
+        do {
+            let decoder = JSONDecoder()
+            let cache = try decoder.decode(Cache.self, from: data)
+            completion(.found(feed: cache.localFeed, timestamp: cache.timeStamp))
+        }catch {
         
-        completion(.found(feed: cache.localFeed, timestamp: cache.timeStamp))
-    }
+            completion(.failure(error))
+          }
+       }
+
     
     func insert(_ feed: [LocalFeedImage],timestamp: Date,completion: @escaping FeedStore.InsertionCompletion){
         let encoder = JSONEncoder()
@@ -117,6 +122,15 @@ class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timeStamp))
     }
     
+    func test_retrieve_deliversErrorOnRetrievalError() {
+        let sut = makeSUT()
+        
+        try! "invalid Data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+        
+        expect(sut, toRetrieve: .failure(anyNSError()))
+    }
+    
+    
     //MARK: Helper
     func makeSUT(file: StaticString = #file,line: UInt = #line) -> CodableFeedStore {
         let sut = CodableFeedStore(storeURL: testSpecificStoreURL())
@@ -147,7 +161,7 @@ class CodableFeedStoreTests: XCTestCase {
         
         sut.retrieve{ retrievedResult in
             switch (expectedResult,retrievedResult) {
-            case (.empty, .empty):
+            case (.empty, .empty), (.failure,.failure):
                 break
                 
             case let (.found(expected),.found(retrieved)):
