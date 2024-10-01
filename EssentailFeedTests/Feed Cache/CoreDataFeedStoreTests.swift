936feacb88eff453ffa6b4ba4445f1f1087e66b3
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2024-10-01 12:27:07 -0400

    Add ManagedCache and ManagedFeedImage model representations.

--- EssentailFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentailFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import EssentailFeed
+import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
     public init() {}
@@ -22,9 +23,19 @@ public final class CoreDataFeedStore: FeedStore {
     public func retrieve(completion: @escaping RetrievalCompletion) {
         completion(.empty)
     }
-    
-    
-    
+}
+
+private class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+private class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
 }
 
 
