f3c7d82e2337afe022c11a87b52c592f379f0bcb
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2024-10-01 12:57:55 -0400

    Load persistent container stores upon CoreDataFeedStore initialization.

--- EssentailFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentailFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -10,7 +10,12 @@ import EssentailFeed
 import CoreData
 
 public final class CoreDataFeedStore: FeedStore {
-    public init() {}
+    
+    private let container: NSPersistentContainer
+    
+    public init(bundle: Bundle = . main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore",in: bundle)
+    }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
@@ -22,6 +27,43 @@ public final class CoreDataFeedStore: FeedStore {
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         completion(.empty)
+    }
+}
+
+private extension NSPersistentContainer {
+    
+    enum LoadingError: Swift.Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Swift.Error)
+    }
+    
+    static func load(modelName name: String,in bundle: Bundle) throws -> NSPersistentContainer{
+        
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        var loadError: Swift.Error?
+        
+        container.loadPersistentStores {
+            loadError = $1
+        }
+        
+        try loadError.map {
+            throw  LoadingError.failedToLoadPersistentStores($0)
+        }
+        
+        return container
+        
+    }
+}
+
+private extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        return bundle.url(forResource: name, withExtension: "momd")
+            .flatMap { NSManagedObjectModel(contentsOf: $0)
+        }
     }
 }
 
@@ -94,7 +136,8 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     
     //MARK: Helper
     private func makeSUT(file: StaticString = #file,line: UInt = #line) -> FeedStore {
-        let sut = CoreDataFeedStore()
+        let storeBundle = Bundle(for: CoreDataFeedStore.self)
+        let sut = try! CoreDataFeedStore(bundle: storeBundle)
         trackForMemoryLeaks(sut, file: file,line: line)
         return sut
     }
