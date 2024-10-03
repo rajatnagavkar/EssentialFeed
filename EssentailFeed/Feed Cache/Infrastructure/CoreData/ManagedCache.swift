b30c43a02e548f339e0b2a69fe0a1100ba6e7602
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2024-10-03 11:40:04 -0400

    Separate Core Data managed class data from  helpers with extension

--- EssentailFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
+++ EssentailFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -11,7 +11,9 @@ import CoreData
  class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
-    
+}
+
+extension ManagedCache {
     
     static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
         let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
@@ -20,7 +22,6 @@ import CoreData
         
         return try context.fetch(request).first
     }
-    
     
     static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
         try find(in: context).map(context.delete)
@@ -32,27 +33,4 @@ import CoreData
     }
 }
 
-@objc(ManagedFeedImage)
- class ManagedFeedImage: NSManagedObject {
-    @NSManaged var id: UUID
-    @NSManaged var imageDescription: String?
-    @NSManaged var location: String?
-    @NSManaged var url: URL
-    @NSManaged var cache: ManagedCache
-    
-    static func images(from localFeed: [LocalFeedImage],in context: NSManagedObjectContext) -> NSOrderedSet {
-        return NSOrderedSet(array:  localFeed.map{ local in
-            let managed  = ManagedFeedImage(context: context)
-            managed.id = local.id
-            managed.imageDescription = local.description
-            managed.location = local.location
-            managed.url = local.url
-            return managed
-        })
-    }
-    
-    var local: LocalFeedImage {
-        return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
-    }
-}
 
