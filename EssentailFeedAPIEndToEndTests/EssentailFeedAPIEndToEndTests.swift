ac793f7f0d06da3eea54933e9d94e9a5ade5bcdb
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2024-10-28 13:47:56 -0400

    Next LoadFeedResult into FeedLoader protocol as "FeedLoader.Result" since they are closely related.

--- EssentailFeedAPIEndToEndTests/EssentailFeedAPIEndToEndTests.swift
+++ EssentailFeedAPIEndToEndTests/EssentailFeedAPIEndToEndTests.swift
@@ -34,14 +34,14 @@ final class EssentailFeedAPIEndToEndTests: XCTestCase {
     }
     
     private func getFeedResult(file: StaticString = #file,
-                               line: UInt = #line) -> LoadFeedResult? {
+                               line: UInt = #line) -> FeedLoader.Result? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHttpClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
         let exp = expectation(description: "Wait for load completion")
-        var receivedResult: LoadFeedResult?
+        var receivedResult: FeedLoader.Result? 
         
         loader.load { result in
             receivedResult = result
