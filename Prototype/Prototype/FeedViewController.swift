56713d39d679238f03eca1874ae1c6e37a7a73b2
Rajat Nagavkar <nagavkarrajat2852@gmail.com>
2024-11-05 15:43:52 -0500

    Fade In animation to stimulate asynchronous loading

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -35,7 +35,8 @@ extension FeedImageCell {
         descriptionLabel.text = model.description
         descriptionLabel.isHidden = model.description == nil
         
-        feedImageView.image = UIImage(named: model.imageName ?? "")
+        fadeIn(UIImage(named: model.imageName ?? ""))
+      
         
     }
 }
